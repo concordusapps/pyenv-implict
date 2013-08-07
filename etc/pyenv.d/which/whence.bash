6804d7620f23e652584bcb9e5bcda1c1ea07534e
Ryan Leckey <rleckey@concordusapps.com>
2013-08-07 02:01:38 -0700

    Initial commit.

---
+++ etc/pyenv.d/which/whence.bash
@@ -0,0 +1,6 @@
+if [ -n "$PYENV_COMMAND" ] && [ ! -x "$PYENV_COMMAND_PATH" ]; then
+  versions=($(pyenv-whence "${PYENV_COMMAND}" 2>/dev/null || true))
+  if [ "${#versions[@]}" -eq 1 ]; then
+    PYENV_COMMAND_PATH="${PYENV_ROOT}/versions/${versions[0]}/bin/${PYENV_COMMAND}"
+  fi
+fi
